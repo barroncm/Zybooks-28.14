61d209c43ce429d61a9a0958dc6787f4ee4a329b
barroncm <167339025+barroncm@users.noreply.github.com>
2024-04-30 11:29:08 -0500

    Update ItemsToPurchase.h
    added step 1 ITP.h requirements

--- ItemsToPurchase.h
+++ ItemsToPurchase.h
@@ -1,1 +1,24 @@
+//Caed Barron; added the Step 1 necessary ItemsToPurchase.h
 
+//written in VSCode then moved to github
+
+#ifndef ITEM_TO_PURCHASE_H
+#define ITEM_TO_PURCHASE_H
+#include <string>
+
+class ItemToPurchase{
+    ItemToPurchase();
+    void setName(string name);
+    void SetCost(double cost);
+    void SetQuantity(int quantity);
+    string GetName();
+    double GetCost();
+    string GetName();
+    int GetQuantity();
+
+    private:
+    string itemDescription = "none";
+    
+};
+
+#endif
