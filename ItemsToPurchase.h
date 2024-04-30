4884721780f85e8806b41b603bd292a08c4d2283
barroncm <167339025+barroncm@users.noreply.github.com>
2024-04-30 18:01:50 -0500

    Update ItemsToPurchase.h

--- ItemsToPurchase.h
+++ ItemsToPurchase.h
@@ -1,23 +1,26 @@
-//Caed Barron; added the Step 1 necessary ItemsToPurchase.h
-
-//written in VSCode then moved to github
-
+//Caed Barron ; work done in VSC and the copied to GitHub and Zybooks
 #ifndef ITEM_TO_PURCHASE_H
 #define ITEM_TO_PURCHASE_H
 #include <string>
+using namespace std;
+
 
 class ItemToPurchase{
+    string name;
+    double cost;
+    int quantity;
+    string itemDescription = "none";    
     ItemToPurchase();
     void setName(string name);
     void SetCost(double cost);
     void SetQuantity(int quantity);
+    void SetDescription(string itemDescription);
     string GetName();
     double GetCost();
-    string GetName();
+    string GetDescription();
     int GetQuantity();
 
     private:
-    string itemDescription = "none";
     
 };
 
