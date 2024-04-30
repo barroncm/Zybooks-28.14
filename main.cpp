45f161b39d9a2ed16f9d5090eaddb617c5bdd3c9
barroncm <167339025+barroncm@users.noreply.github.com>
2024-04-30 18:00:11 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,19 @@
-// Caed Barron; second part of the main; worked on VSC and then copied to Github
-// throwing error in VSC -> need to check
-
-
+//Caed Barron ; work done in VSC and the copied to GitHub and Zybooks
 #include <iostream>
 #include <iomanip>
 #include <string>
 using namespace std;
 #include "ItemToPurchase.h"
-#include "ShoppingCart.h"
-string customerName;
-string currentDate;
+#include "shoppingcart.h"
 
-    cout << "Enter customer's name:" << endl;
-   cin >> customerName;
-   cout << "Enter today's date:" << endl;
-   cin >> currentDate;
+
+    
 
 void PrintMenu() {
    /* Type your code here */
    //given by zybooks instructions
-   cout << "a - Add item to cart" << endl;
-   cout <<"d - Remove item from cart" << endl;
+    cout << "a - Add item to cart" << endl;
+    cout <<"d - Remove item from cart" << endl;
     cout << "c - Change item quantity" << endl;
     cout << "i - Output items' descriptions" << endl;
     cout << "o - Output shopping cart" << endl;
@@ -28,69 +21,82 @@ void PrintMenu() {
 
    
 }
-void ExecuteMenu(char option, ShoppingCart& theCart) {
+//given line below from zybooks
+void ExecuteMenu(char selection, ShoppingCart& theCart) {
    /* Type your code here */
    string itemName;
    string itemDescription;
    double itemCost;
    int itemQuantity;
-   cin >> option;
-   if (option == "a"){
-    cout << "ADD ITEM TO CART" << endl;
-    cout << "Enter the item name" << endl;
-    getline(cin, itemName);
-    cout << "Enter the item description" << endl;
-    getline(cin, itemDescription);
-    cout << "Enter the item price:" << endl;
-    getline(cin, itemCost);
-    cout << "Enter the item quantity" << endl;
-    cin >> itemQuantity;
-    AddItem().SetName(itemName);
-    AddItem().SetQuantity(itemQuantity);
-    AddItem().SetDescription(itemDescription);
-    AddItem().SetCost(itemCost);
 
-   } else if (option == "d")
-   { cout << "REMOVE ITEM FROM CART" << endl;
-   cout << "Enter name of item to remove:" << endl;
-   getline(cin, itemName);
-   RemoveItem().itemName;
+   cin >> selection;
+
+    switch(selection){
+        case 'a' : {
+            cout << "ADD ITEM TO CART" << endl;
+            cout << "Enter the item name" << endl;
+            getline(cin, itemName);
+             cout << "Enter the item description" << endl;
+            getline(cin, itemDescription);
+             cout << "Enter the item price:" << endl;
+            cin >> itemCost;
+            cout << "Enter the item quantity" << endl;
+            cin >> itemQuantity;
     
-   }else if (option == "C")
-   { cout << "CHANGE ITEM QUANTITY" << endl;
-   cout << "Enter the item name:" << endl;
-   getline(cin, itemName);
-   cout << "Enter the new quantity" << endl;
-   cin >> itemQuantity;
-    ModifyItem().SetQuantity(itemQuantity);
-   } else if (option == "i")
-   {
-    PrintDescription(theCart);
+            ItemToPurchase nextItem;
+            nextItem.SetName(itemName);
+            nextItem.SetDescription(itemDescription);
+            nextItem.SetCost(itemCost);
+            nextItem.SetQuantity(itemQuantity);
+            theCart.AddItem(nextItem);
+
+        }
+        case 'd' : {
+            cout << "REMOVE ITEM FROM CART" << endl;
+            cout << "Enter name of item to remove:" << endl;
+          
+            getline(cin, itemName);
+            theCart.RemoveItem(itemName);
+            }
+        case 'c' : {
+            cout << "CHANGE ITEM QUANTITY" << endl;
+            cout << "Enter the item name:" << endl;
+            getline(cin, itemName);
+            cout << "Enter the new quantity" << endl;
+            cin >> itemQuantity;
+            ModifyItem().SetQuantity(itemQuantity);
+        }
+        case 'i' : {
+            PrintDescription(theCart);
+        }
+        
+        case 'q' : {
+            cout << "QUIT" << endl;
+        }
+        //placeholder...will have to check what zybooks wants
+        default : {
+            cout << "Invalid entry" << endl;
+        }
+        }
     
-   }else if (option == "o")
-   {
-    PrintTotal(theCart);
-   } else if(option =="q"){
-    cout << "QUIT" << endl;
-   } else{
-    cout << "invaldi entry" << endl;
-   }
-   
-   
-   
-   
-   
-}
+    }
 
 int main() {
    /* Type your code here */
+   string customerName;
+    string currentDate;
+   cout << "Enter customer's name:" << endl;
    getline(cin, customerName);
+   cout << "Enter today's date:" << endl;
    getline(cin, currentDate);
     ShoppingCart theCart(customerName, currentDate);
-    while(char option != "q"){
+    char option;
+    cin >> option;
+    //have to use single quotes here
+    while(option != 'q'){
         PrintMenu();
         cin >> option;
-        ExceuteMenu(option, theCart);
+        ExecuteMenu(option, theCart);
 
     }
   
